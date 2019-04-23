fd2695730a973238c5b091b6b3c30d44d6365814
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2019-04-23 08:47:37 +0530

    Update os module.py

--- os module.py
+++ os module.py
@@ -3,3 +3,9 @@ import os
 f=open("mom.txt","w")
 f.close()
 os.remove("mom.txt")
+#if fie exists then delete
+if os.path.exists("ma.txt"):    # when executed second time then prints The file does not exist on the console screen
+    os.remove("ma.txt")
+else:
+    print("The file does not exist")
+
